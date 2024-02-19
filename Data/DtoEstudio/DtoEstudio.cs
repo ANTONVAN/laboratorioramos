2ceda193d33051860f9febe8ce845e6c2ec605be
Omar Alvarez Alcántara <omaralvarez01.oaa@gmail.com>
2024-02-18 21:45:33 -0600

    Consulta de gráficas
    Cambios que incluyen la consulta de gráficas para pacientes, Médicos y Empresas
    
    Co-Authored-By: Omar Alvarez Alcántara <32376893+omarin39@users.noreply.github.com>

---
+++ Data/DtoEstudio/DtoEstudio.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography;
+
+namespace LaboratorioRamos.Data.DtoEstudio
+{
+    public class DtoEstudio
+    {
+        public List<DtoResultGral> Results{ get; set; }
+    }
+
+    public class DtoResultGral
+    {
+        public string Id {get; set; }
+        public string? Clave {get; set; }
+        public string? Paciente {get; set; }
+        public Int32 Edad {get; set; }
+        public string? Genero {get; set; }
+        public Int32 ReuqestStudyId { get; set; }
+        public string ExpedienteId { get; set; }
+        public string? NombreEstudio { get; set; }
+        public string? HoraSolicitud { get; set; }
+        public List<DtoParameters>? Parameters { get; set; }
+
+    }
+    public class DtoParameters
+    {
+        public string Id { get; set; }
+        public string? TipoValor { get; set; }
+        public Int32 EstudioId { get; set; }
+        public Int32 SolicitudEstudioId { get; set; }
+        public string? Nombre { get; set; }
+        public string? Clave { get; set; }
+        public string? Unidades { get; set; }
+        public string? Etiqueta { get; set; }
+        public string? Valor { get; set; }
+        
+
+    }
+
+    public class DtoEstudioGrafica
+    {
+        public string Fecha { get; set; }
+        public string Estudio { get; set; }
+        public string? Valor { get; set; }
+    }
+}
---
+++ Data/DtoEstudio/DtoRequestEstudio/DtoRequestEstudio.cs
@@ -0,0 +1,9 @@
+﻿namespace LaboratorioRamos.Data.DtoEstudio.DtoRequestEstudio
+{
+    public class DtoRequestEstudio
+    {
+        public List<string> Fecha { get; set; }
+        public string Buscar { get; set; }
+        public List<int> Estudio { get; set; }
+    }
+}
