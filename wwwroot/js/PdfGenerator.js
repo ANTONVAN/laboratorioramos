b20b586393cde8df830562d6276f46f6d866d92b
Omar Alvarez Alcántara <omaralvarez01.oaa@gmail.com>
2024-04-01 17:43:18 -0600

    New Print Result
    New Print Result

--- wwwroot/js/PdfGenerator.js
+++ wwwroot/js/PdfGenerator.js
@@ -9,9 +9,36 @@
 
 function ViewPDF(iframeID, byteBase64) {
     document.getElementById(iframeID).innerHTML = "";
+    var contentType = 'application/pdf';
+    var blob = b64toBlob(byteBase64, contentType);
+    var blobUrl = URL.createObjectURL(blob);
     var ifra = document.createElement('iframe');
-    ifra.setAttribute("src", "data:application/pdf;base64," + byteBase64);
+    ifra.setAttribute("src", blobUrl);
     ifra.style.with = "100%";
     ifra.style.height = "680%";
     document.getElementById(iframeID).appendChild(ifra);
+}
+
+function b64toBlob(b64Data, contentType, sliceSize) {
+    contentType = contentType || '';
+    sliceSize = sliceSize || 512;
+
+    var byteCharacters = atob(b64Data);
+    var byteArrays = [];
+
+    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+        var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+        var byteNumbers = new Array(slice.length);
+        for (var i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+        var byteArray = new Uint8Array(byteNumbers);
+
+        byteArrays.push(byteArray);
+    }
+
+    var blob = new Blob(byteArrays, { type: contentType });
+    return blob;
 }
