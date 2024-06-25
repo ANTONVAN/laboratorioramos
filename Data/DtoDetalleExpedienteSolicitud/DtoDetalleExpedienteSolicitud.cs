51d985bedd767c5acfaaf212f7ffe58f4c4a03fe
Omar Alvarez Alcántara <omaralvarez01.oaa@gmail.com>
2024-06-25 11:02:39 -0600

    Validacion Login Paciente
    Validacion Login Paciente

---
+++ Data/DtoDetalleExpedienteSolicitud/DtoDetalleExpedienteSolicitud.cs
@@ -0,0 +1,9 @@
+﻿namespace LaboratorioRamos.Data.DtoDetalleExpedienteSolicitud
+{
+    public class DtoDetalleExpedienteSolicitud
+    {
+        public string SolicitudId { get; set; }
+        public string ExpedienteId { get; set; }
+        public string Clave { get; set; }
+    }
+}
