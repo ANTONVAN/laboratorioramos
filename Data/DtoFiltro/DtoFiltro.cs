5c03e3ffb69b0faa60a4ed06d4d2e033edf3a056
Omar Alvarez Alcántara <128910821+omarinalvarez01@users.noreply.github.com>
2025-03-05 23:35:31 -0600

    Update Arbol
    Update Arbol

--- Data/DtoFiltro/DtoFiltro.cs
+++ Data/DtoFiltro/DtoFiltro.cs
@@ -10,9 +10,9 @@ namespace LaboratorioRamos.Data.DtoFiltro
         [RegularExpression("^[0-9]*$", ErrorMessage = "Solo se permiten números")]
         public string? Solicitud { get; set; }
         //[Required(ErrorMessage = "Fecha Inicial es requerida")]
-        public DateTime? DateInicial { get; set; } = DateTime.Now;
+        public DateTime? DateInicial { get; set; } //= DateTime.Now;
         //[Required(ErrorMessage = "Fecha Final es requerida")]
-        public DateTime? DateFinal { get; set; } = DateTime.Now;
+        public DateTime? DateFinal { get; set; } //= DateTime.Now;
     }
 
     public class DtoFiltroLst
