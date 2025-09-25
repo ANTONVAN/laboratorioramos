7f45c0dc0adafb89d492fcf1b789ea2b0f13f4dc
Omar Alvarez Alcántara <128910821+omarinalvarez01@users.noreply.github.com>
2025-09-24 21:56:27 -0600

    Actualizacion descarga estudios

--- Data/DtoExpediente/DtoExpediente.cs
+++ Data/DtoExpediente/DtoExpediente.cs
@@ -86,26 +86,26 @@
     public class DtoInfoExpediente
     {
         public string Id { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public string Expediente { get; set; }
-        public string Sexo { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? Expediente { get; set; }
+        public string? Sexo { get; set; }
         public DateTime FechaNacimiento { get; set; }
-        public string FechaNacimientoFormat { get; set; }
-        public int Edad { get; set; }
-        public string Telefono { get; set; }
-        public string Correo { get; set; }
-        public string Cp { get; set; }
-        public string Estado { get; set; }
-        public string Municipio { get; set; }
-        public string Celular { get; set; }
-        public string Calle { get; set; }
-        public string Observaciones { get; set; }
-        public string Colonia { get; set; }
-        public string Sucursal { get; set; }
-        public string UsuarioId { get; set; }
-        public bool HasWallet { get; set; }
-        public double Wallet { get; set; }
+        public string? FechaNacimientoFormat { get; set; }
+        public int Edad { get; set; } = 0;
+        public string? Telefono { get; set; }
+        public string? Correo { get; set; }
+        public string? Cp { get; set; }
+        public string? Estado { get; set; }
+        public string? Municipio { get; set; }
+        public string? Celular { get; set; }
+        public string? Calle { get; set; }
+        public string? Observaciones { get; set; }
+        public int? Colonia { get; set; }
+        public string? Sucursal { get; set; }
+        public string? UsuarioId { get; set; }
+        public bool HasWallet { get; set; } = false;
+        public double Wallet { get; set; } = 0;
         public DateTime FechaActivacionMonedero { get; set; }
         public List<TaxDatum> TaxData { get; set; }
     }
